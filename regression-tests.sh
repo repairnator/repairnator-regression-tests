1639b2b58b165d3123cdc76b6ee0ee41bbdf095a
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-25 12:25:08 +0100

    @monperrus at work

--- regression-tests.sh
+++ regression-tests.sh
@@ -11,11 +11,11 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.inria.repairnator&a=repairnator-pipeline&v=LATEST&c=jar-with-dependencies"
 
 # toy project failingTest NpeFix
-java -jar repairnator-pipeline.jar --repairTools NPEFix -b 564711868 | grep 'PIPELINE FINDING: PATCHED'
+$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 564711868 | grep 'PIPELINE FINDING: PATCHED'
 
 # donnelldebnam/CodeU-Spring-2018-29#59 (June 25 2018, NPEFix, merged)
-java -jar repairnator-pipeline.jar --repairTools NPEFix -b 395891390 | grep 'PIPELINE FINDING: PATCHED'
+$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 395891390 | grep 'PIPELINE FINDING: PATCHED'
 
 # parkito/BasicDataStructuresAndAlgorithms#3 (Mar 21 2018, merged)
-java -jar repairnator-pipeline.jar --repairTools NPEFix -b 355923834 | grep 'PIPELINE FINDING: PATCHED'
+$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 355923834 | grep 'PIPELINE FINDING: PATCHED'
 
