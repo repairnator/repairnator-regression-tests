8dd5e54535cbb112b7ccc914c551246b689b45ce
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-25 12:10:54 +0100

    @monperrus at work

--- regression-tests.sh
+++ regression-tests.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # run regression tests of repairnator
 
+# with those two options we will catch problems with grep
+set -o pipefail
 set -e
 
 # download the latest version
 curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.inria.repairnator&a=repairnator-pipeline&v=LATEST&c=jar-with-dependencies"
 
-# 01 failing test
-java -jar repairnator-pipeline.jar -b 564711868
-ls
+# toy project failingTest NpeFix
+java -jar repairnator-pipeline.jar --repairTools NPEFix -b 564711868 | grep 'PIPELINE FINDING: PATCHED'
+
+# donnelldebnam/CodeU-Spring-2018-29#59 (June 25 2018, NPEFix, merged)
+java -jar repairnator-pipeline.jar --repairTools NPEFix -b 395891390 | grep 'PIPELINE FINDING: PATCHED'
+
+# parkito/BasicDataStructuresAndAlgorithms#3 (Mar 21 2018, merged)
+java -jar repairnator-pipeline.jar --repairTools NPEFix -b 355923834 | grep 'PIPELINE FINDING: PATCHED'
+
