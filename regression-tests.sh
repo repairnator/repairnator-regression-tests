582ccda76e0ddc4d5859a5bd072e6e1ab252958f
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-25 12:39:41 +0100

    @monperrus at work

--- regression-tests.sh
+++ regression-tests.sh
@@ -10,12 +10,14 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 # download the latest version
 curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.inria.repairnator&a=repairnator-pipeline&v=LATEST&c=jar-with-dependencies"
 
-# toy project failingTest NpeFix
+# each task must take less than 10 minutes otherwise the travis build fails
+
+echo "toy project failingTest NpeFix"
 $JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 564711868 | grep 'PIPELINE FINDING: PATCHED'
 
-# donnelldebnam/CodeU-Spring-2018-29#59 (June 25 2018, NPEFix, merged)
+echo "donnelldebnam/CodeU-Spring-2018-29#59 (June 25 2018, NPEFix, merged)"
 $JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 395891390 | grep 'PIPELINE FINDING: PATCHED'
 
-# parkito/BasicDataStructuresAndAlgorithms#3 (Mar 21 2018, merged)
+echo "parkito/BasicDataStructuresAndAlgorithms#3 (Mar 21 2018, merged)"
 $JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 355923834 | grep 'PIPELINE FINDING: PATCHED'
 
