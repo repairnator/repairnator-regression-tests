a4cb1a2731a7fa97c1b11b2da06ab32997df2721
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-26 11:32:48 +0100

    @monperrus at work

--- regression-tests.sh
+++ regression-tests.sh
@@ -13,17 +13,19 @@ curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.
 # each task must take less than 10 minutes otherwise the travis build fails
 
 echo "toy project failingTest NpeFix"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 564711868 | tee output.log
+$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 564711868 | tee output.log
 grep 'PIPELINE FINDING: PATCHED' output.log
 
 echo "donnelldebnam/CodeU-Spring-2018-29#59 (June 25 2018, NPEFix, merged)"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 395891390 | tee output.log
+$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 395891390 | tee output.log
 grep 'PIPELINE FINDING: PATCHED' output.log
 
 echo "parkito/BasicDataStructuresAndAlgorithms#3 (Mar 21 2018, merged)"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --repairTools NPEFix -b 355923834 | tee output.log
+$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 355923834 | tee output.log
 grep 'PIPELINE FINDING: PATCHED' output.log
 
-echo "Defects4J Math70 AstorJGenProg"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar -b 155069540 --repairTools AstorJGenProg | tee output.log
-grep 'PIPELINE FINDING: PATCHED' output.log
+## does not work
+# echo "Defects4J Math70 AstorJGenProg (from paper)"
+# $JAVA_HOME/bin/java -jar repairnator-pipeline.jar--ghOauth $GITHUB_TOKEN  --repairTools AstorJGenProg -b 155069540  | tee output.log
+# grep 'PIPELINE FINDING: PATCHED' output.log
+
