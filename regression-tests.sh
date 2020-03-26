4a7637b1c2a721ccbc7f1d7ca62ee0ffa63192be
Martin Monperrus <martin.monperrus@gnieh.org>
2020-03-26 12:48:42 +0100

    @monperrus at work

--- regression-tests.sh
+++ regression-tests.sh
@@ -15,25 +15,27 @@ curl -Lo repairnator-pipeline.jar "https://search.maven.org/remote_content?g=fr.
 # each task must take less than 10 minutes otherwise the travis build fails
 
 echo "toy project failingTest NpeFix"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 564711868 | tee output.log
+time $JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 564711868 | tee output.log
 grep 'PIPELINE FINDING: PATCHED' output.log
 
 echo "donnelldebnam/CodeU-Spring-2018-29#59 (June 25 2018, NPEFix, merged)"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 395891390 | tee output.log
+time $JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 395891390 | tee output.log
 grep 'PIPELINE FINDING: PATCHED' output.log
 
 echo "parkito/BasicDataStructuresAndAlgorithms#3 (Mar 21 2018, merged)"
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 355923834 | tee output.log
+time $JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NPEFix -b 355923834 | tee output.log
 grep 'PIPELINE FINDING: PATCHED' output.log
-
 
 ###############  NOPOL #####################
 # now we test Nopol
-$JAVA_HOME/bin/java -jar repairnator-pipeline.jar --ghOauth $GITHUB_TOKEN --repairTools NopolAllTests -b 346537408 | tee output.log
+
+# reproducing https://github.com/Spirals-Team/open-science-repairnator/blob/master/data/expedition-2/374841318.html (a Kali patch)
+$JAVA_HOME/bin/java -cp $JAVA_HOME/lib/tools.jar:repairnator-pipeline.jar fr.inria.spirals.repairnator.pipeline.Launcher --ghOauth $GITHUB_TOKEN --repairTools NopolAllTests -b 374841318 | tee output.log
+grep 'PIPELINE FINDING: PATCHED' output.log
 
 
 
-## does not work
+## does not work, bug in Astor?
 # echo "Defects4J Math70 AstorJGenProg (from paper)"
 # $JAVA_HOME/bin/java -jar repairnator-pipeline.jar--ghOauth $GITHUB_TOKEN  --repairTools AstorJGenProg -b 155069540  | tee output.log
 # grep 'PIPELINE FINDING: PATCHED' output.log
